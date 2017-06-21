916121ff553f1c78af0e18aee57def5f9b30c877
sakakendo0321 <sakakendo0321@gmail.com>
2017-06-21 14:52:12 +0900

    hello.java
    test for java

---
+++ java/hello.java
@@ -0,0 +1,12 @@
+
+//import java.lang.System;
+//import java.io.Buff0ierReader;
+import java.io.InputStreamReader;
+
+public class hello{
+  public static void main(String[] args){
+      System.out.println("Hello world");
+      System.out.println("Hello java world");
+
+  }
+}
